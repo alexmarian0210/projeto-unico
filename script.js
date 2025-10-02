34ac373e9604cb638c88fa4f49a61fb5f88f765f
alexmarian0210 <alex_marian@estudante.sesisenai.org.b>
2025-10-02 20:33:10 -0300

    começo

---
+++ script.js
@@ -0,0 +1,21 @@
+/*
+
+const transacao = {
+    id: 1,
+    tipo: "despesa", // receita, despesa
+    valor: 45.90,
+    categoria: "alimentação",
+    descricao: "Almoço no restaurante",
+    data: "2024-01-20",
+    formaPagamento: "cartão",
+    recorrente: false
+};
+
+const meta = {
+    categoria: "lazer",
+    valorLimite: 300,
+    valorGasto: 150,
+    mes: "2024-01"
+};
+*/
+
